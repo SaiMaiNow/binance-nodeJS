dd3a7d9930d73a736986ae91d2c8eb31f2652244
Thanatchon Zirinawaranoent <tanatchon_ph@cmu.ac.th>
2025-09-10 11:52:31 +0700

    เพิ่ม library ที่ใช้และปรับ PORT ให้รันได้เลยตอน npm run dev

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use('/api', require('./routes/app'));
 
 app.listen(PORT, () => {
   createPool(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST, process.env.DB_PORT, process.env.DB_NAME);
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}/api/`);
 });

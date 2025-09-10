bd4fd641e5f0b47e72485cb3a3398105a3871075
Thanatchon Zirinawaranoent <tanatchon_ph@cmu.ac.th>
2025-09-10 11:54:21 +0700

    ทำให้ app.js รับ api จาก /cryptos โดยเรียกใช้งานตอนใน ./cryptos.routes อีกทีเป็นการแยก Router

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require('path');
 const router = express.Router();
+
 
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+router.use('/cryptos', require('./cryptos.routes'));
 
 module.exports = router;

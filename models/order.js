94c2e4e713a944b3649bae0ca60a61e526cceb92
AutCraft <86147510+AutCraft@users.noreply.github.com>
2025-09-14 15:06:32 +0700

    Update order.js Add id, userid, cryptoid In Table Db
    เพิ่ม id, userid, cryptoid ใน Table db

--- models/order.js
+++ models/order.js
@@ -3,6 +3,19 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../function/postgre');
 
 const Order = sequelize.define('order', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    userid: {
+        type: DataTypes.INTEGER,
+        references: { model: 'users', key: 'id' }
+    },
+    cryptoid: {
+        type: DataTypes.INTEGER,
+        references: { model: 'crypto', key: 'id' }
+    },
     amount: {
         type: DataTypes.FLOAT
     },

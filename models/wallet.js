1dbb788c89cb8618616eed68e41e01a61a379512
AutCraft <86147510+AutCraft@users.noreply.github.com>
2025-09-14 15:11:33 +0700

    Update wallet.js Add userid, cryptoid In Table Db models
    เพิ่ม userid, cryptoid ใน Table db models แล้วแก้ ไม่ให้ null ได้

--- models/wallet.js
+++ models/wallet.js
@@ -3,11 +3,25 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../function/postgre');
 
 const Wallet = sequelize.define('wallet', {
+    userid:{
+        type: DataTypes.INTEGER,
+        references: { model: 'users', key: 'id' },
+        primaryKey: true,
+        allowNull: false,
+    },
+    cryptoid:{
+        type: DataTypes.INTEGER,
+        references: { model: 'crypto', key: 'id' },
+        primaryKey: true,
+        allowNull: false,
+    },
     amount:{
-        type: DataTypes.FLOAT
+        type: DataTypes.FLOAT,
+        allowNull: false,
     },
     price:{
-        type: DataTypes.FLOAT
+        type: DataTypes.FLOAT,
+        allowNull: false,
     }
 });
 

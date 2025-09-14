ebd5b396cee3675ab19ebea08d6ed7e2f2d46763
Rachanon (RCN) <143895811+SaiMaiNow@users.noreply.github.com>
2025-09-14 15:47:52 +0700

    ปรับปรุงไฟล์ users.js โดยเปลี่ยนจากการใช้ getPool() เป็นการใช้โมเดล Users ในการจัดการการลงทะเบียนและอัปเดตข้อมูลผู้ใช้ รวมถึงปรับปรุงการตรวจสอบและการสร้างข้อมูลให้มีความถูกต้องและมีประสิทธิภาพมากขึ้น

--- routes/router/users.js
+++ routes/router/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getPool } = require('../../function/postgre');
+const { Users } = require('../../models/users');
 
 router.post("/register", async (req, res) => {
   const { fName, lName, email, password, tel, birthday } = req.body;
@@ -10,15 +10,13 @@ router.post("/register", async (req, res) => {
     if (!fName || !lName || !tel || !email || !password || !birthday) return res.status(400).json({ message: "No fields to register" });
 
     if(!validateBirthday(birthday)) return res.status(400).json({ message: "Invalid birthday" });
-
-    const pool = getPool();
-    const selectQuery = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-    if (selectQuery.rowCount > 0) {
+    const selectQuery = await Users.findOne({ where: { email } });
+    if (selectQuery) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const result = await pool.query('INSERT INTO users ("firstName", "lastName", email, password, tel, birthday) VALUES ($1, $2, $3, $4, $5, $6)', [fName, lName, email, password, tel, birthday]);
-    if (result.rowCount === 0) {
+    const result = await Users.create({ firstName: fName, lastName: lName, email, password, tel, birthday });
+    if (!result) {
       return res.status(400).json({ message: "User registration failed" });
     }
 
@@ -42,17 +40,16 @@ router.put("/update", async (req, res) => {
     const userCache = req.session.user;
     if (!userCache?.email) return res.status(400).json({ message: "User is not Logged in" });
 
-    const pool = getPool();
-    const userQuery = await pool.query('SELECT "firstName", "lastName", "tel", "email", "password" FROM users WHERE email = $1', [userCache.email]);
-    if (userQuery.rowCount === 0) {
+    const userQuery = await Users.findOne({ where: { email: userCache.email } });
+    if (!userQuery) {
       return res.status(400).json({ message: "User not found" });
     }
 
-    let NewFName = userQuery.rows[0].firstName;
-    let NewLName = userQuery.rows[0].lastName;
-    let NewTel = userQuery.rows[0].tel;
-    let NewEmail = userQuery.rows[0].email;
-    let NewPassword = userQuery.rows[0].password;
+    let NewFName = userQuery.firstName;
+    let NewLName = userQuery.lastName;
+    let NewTel = userQuery.tel;
+    let NewEmail = userQuery.email;
+    let NewPassword = userQuery.password;
 
     if (fName) NewFName = fName;
     if (lName) NewLName = lName;
@@ -60,12 +57,12 @@ router.put("/update", async (req, res) => {
     if (email) NewEmail = email;
     if (password) NewPassword = password;
 
-    const updateQuery = await pool.query('UPDATE users SET "firstName" = $1, "lastName" = $2, "tel" = $3, "email" = $4, "password" = $5 WHERE email = $6', [NewFName, NewLName, NewTel, NewEmail, NewPassword, userCache.email]);
-    if (updateQuery.rowCount === 0) {
+    const updateQuery = await Users.update({ firstName: NewFName, lastName: NewLName, tel: NewTel, email: NewEmail, password: NewPassword }, { where: { email: userCache.email } });
+    if (!updateQuery) {
       return res.status(400).json({ message: "User update failed" });
     }
 
-    return res.status(200).json({ message: "User updated successfully", user: { fName: NewFName, lName: NewLName, tel: NewTel, email: NewEmail, password: NewPassword } });
+    return res.status(200).json({ message: "User updated successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
@@ -109,8 +106,6 @@ router.post('/login', async (req, res) => {
     console.error('Login Error:', err);
     res.status(500).json({message: 'Internal server error' });
   }
-  
-
 });
 
 

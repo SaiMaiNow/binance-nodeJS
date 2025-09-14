c1cb55d71734efb56e678994a76f6d9ee5dd6034
AutCraft <86147510+AutCraft@users.noreply.github.com>
2025-09-14 15:07:39 +0700

    Update crypto.js Add id in Table DB
    เพิ่ม id ใน Table db

--- models/crypto.js
+++ models/crypto.js
@@ -3,6 +3,11 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../function/postgre');
 
 const Crypto = sequelize.define('crypto', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
     cryptoname: {
         type: DataTypes.STRING
     }
